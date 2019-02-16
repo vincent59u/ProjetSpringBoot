26f2e7b44307aec5d223ff95add0b2d07e5880f4
Matthieu VINCENT <contact.matthieu.vincent@gmail.com>
2019-02-16 09:42:03 +0100

    rm les fichiers .gitignore

--- taches-service/target/classes/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO tache(id, nom, responsable_id, date_debut, date_echeance, etat)
-VALUES('de7d9052-4961-4b4f-938a-3cd12cbe1f82', 'tache1', '425e7701-02c6-4de3-9333-a2459eece1c8', '2020-01-01', '2020-01-03', '0');
-
-INSERT INTO tache_participants_id VALUES ('de7d9052-4961-4b4f-938a-3cd12cbe1f82', 'de7f2052-4961-4b4f-938c-3cd12clz9f82');
-INSERT INTO tache_participants_id VALUES ('de7d9052-4961-4b4f-938a-3cd12cbe1f82', 'e60fe9dd-24c5-41b3-8379-ab7172c2ad16');
