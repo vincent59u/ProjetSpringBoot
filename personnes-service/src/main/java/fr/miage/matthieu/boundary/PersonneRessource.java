722dd25db2ea0945bf98c3d6937ea2be6b322688
Matthieu VINCENT <contact.matthieu.vincent@gmail.com>
2019-02-27 00:08:22 +0100

    Dockerisation + Modification de personnes-services pour mongodb

--- personnes-service/src/main/java/fr/miage/matthieu/boundary/PersonneRessource.java
+++ personnes-service/src/main/java/fr/miage/matthieu/boundary/PersonneRessource.java
@@ -1,8 +1,9 @@
 package fr.miage.matthieu.boundary;
 
 import fr.miage.matthieu.entity.Personne;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface PersonneRessource extends CrudRepository<Personne,String> {
+public interface PersonneRessource extends MongoRepository<Personne,String> {
 
 }
--- personnes-service/src/main/java/fr/miage/matthieu/entity/Personne.java
+++ personnes-service/src/main/java/fr/miage/matthieu/entity/Personne.java
@@ -1,9 +1,9 @@
 package fr.miage.matthieu.entity;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.format.annotation.DateTimeFormat;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
@@ -11,7 +11,7 @@ import javax.validation.constraints.Size;
 import java.io.Serializable;
 import java.util.Date;
 
-@Entity
+@Document(collection = "personnes")
 public class Personne implements Serializable {
 
     @Id
