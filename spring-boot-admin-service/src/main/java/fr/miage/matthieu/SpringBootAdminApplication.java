d3e8f77a1cbc6f32eafbbc6b03a2c5a8b04f72ee
Matthieu VINCENT <contact.matthieu.vincent@gmail.com>
2019-03-03 16:27:31 +0100

    Configuration de Spring-boot-admin

--- spring-boot-admin-service/src/main/java/fr/miage/matthieu/SpringBootAdminApplication.java
+++ spring-boot-admin-service/src/main/java/fr/miage/matthieu/SpringBootAdminApplication.java
@@ -2,12 +2,14 @@ package fr.miage.matthieu;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@EnableAutoConfiguration
 @EnableDiscoveryClient
 @EnableAdminServer
-@SpringBootApplication
 public class SpringBootAdminApplication {
 
     public static void main(String[] args) {
