abcba56bd3d45d658eef243cb159fc7c58ca426b
Matthieu VINCENT <contact.matthieu.vincent@gmail.com>
2019-03-13 14:30:05 +0100

    Modification du script init.sql de taches-service

--- taches-service/init_postgres_db.sql
+++ taches-service/init_postgres_db.sql
@@ -1,3 +1,5 @@
+TRUNCATE TABLE tache CASCADE;
+
 INSERT INTO tache(id, nom, responsable_id, date_debut, date_echeance, etat) VALUES('de7d9052-4961-4b4f-938a-3cd12cbe1f82', 'tache1', '425e7701-02c6-4de3-9333-a2459eece1c8', NOW(), '2019-05-30', '1');
 INSERT INTO tache_participants_id VALUES ('de7d9052-4961-4b4f-938a-3cd12cbe1f82', 'de7f2052-4961-4b4f-938c-3cd12clz9f82');
 INSERT INTO tache_participants_id VALUES ('de7d9052-4961-4b4f-938a-3cd12cbe1f82', 'e60fe9dd-24c5-41b3-8379-ab7172c2ad16');
