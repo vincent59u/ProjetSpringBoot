7c23fab0d0aa66bfe0491c9b3de2d314c145190d
Matthieu VINCENT <contact.matthieu.vincent@gmail.com>
2019-03-17 22:59:50 +0100

    Ajout des scripts de peuplement des BDD dans la livraison

---
+++ LIVRAISON/personnes-service/init_mongo_db.js
@@ -0,0 +1,7 @@
+db.personnes.drop();
+db.personnes.insertMany([
+    {"_id": "425e7701-02c6-4de3-9333-a2459eece1c8", "nom": "GASIOROWSKI", "prenom": "Loic", "email": "loic.gasiorowski@gmail.com", "mdp": "motdepasse", "date_naissance": new Date("1996-07-31"), "commune": "Moyeuvre-grande", "codepostal": "57250"},
+    {"_id": "de7f2052-4961-4b4f-938c-3cd12clz9f82", "nom": "GRANDJEAN", "prenom": "Rudy", "email": "rudy.grandjean@gmail.com", "mdp": "motdepasse", "date_naissance": new Date("1996-06-15"), "commune": "Laneuveville", "codepostal": "54410"},
+    {"_id": "e60fe9dd-24c5-41b3-8379-ab7172c2ad16", "nom": "VINCENT", "prenom": "Matthieu", "email": "matthieu.vincent@gmail.com", "mdp": "motdepasse", "date_naissance": new Date("1995-03-31"), "commune": "Rupt-sur-Moselle", "codepostal": "88360"},
+    {"_id": "0e9d45d5-40c6-4b90-bbee-65147e31c9a2", "nom": "DESSI", "prenom": "Maxime", "email": "maxime.dessi@gmail.com", "mdp": "motdepasse", "date_naissance": new Date("1996-07-27"), "commune": "Nancy", "codepostal": "54000"}
+]);
